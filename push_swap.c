d28098928daa3c7bdea58ee7878bc457643c255f
biballo <dmoraleda11@gmail.com>
2024-12-04 11:56:52 +0100

    dsadsa

--- push_swap.c
+++ push_swap.c
@@ -6,67 +6,78 @@
 /*   By: dmoraled <dmoraled@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 10:35:38 by dmoraled          #+#    #+#             */
-/*   Updated: 2024/12/04 11:13:09 by dmoraled         ###   ########.fr       */
+/*   Updated: 2024/12/04 11:56:44 by dmoraled         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
+#include "push_swap.h"
 
 int	lst_less(t_list *a, t_list *b)
 {
 	return (*(int *)(a->content) < *(int *)(b->content));
 }
 
-int	lst_is_sorted(t_list *lst, int (*less)(t_list *, t_list *))
-{
-	if (!lst)
-		return (1);
-	while (lst->next)
-	{
-		if (!less(lst, lst->next))
-			return (0);
-		lst = lst->next;
-	}
-	return (1);
-}
-
-void	lst_bubble(t_list *lst, int (*less)(t_list *, t_list *))
+int	lst_min(t_list *lst, int threshold)
 {
 	t_list	*l;
-	void	*tmp;
+	int		min;
+	int		min_idx;
+	t_item	*item;
+	int		curr_idx;
 
-	if (!lst)
-		return ;
-	while (1)
+	min = INT_MAX;
+	min_idx = -1;
+	curr_idx = 0;
+	l = lst;
+	while (l)
 	{
-		if (lst_is_sorted(lst, less))
-			return ;
-		l = lst;
-		while (l->next)
+		item = (t_item *)l->content;
+		if (item->value < min && item->value > threshold)
 		{
-			if (!less(l, l->next))
-			{
-				tmp = l->next->content;
-				l->next->content = l->content;
-				l->content = tmp;
-			}
-			l = l->next;
+			min = item->value;
+			min_idx = curr_idx;
 		}
+		l = l->next;
+		++curr_idx;
+	}
+	return (min_idx);
+}
+
+void	lst_fill_indices(t_list *lst)
+{
+	t_item	*item;
+	int		next_min_idx;
+	int		threshold;
+	int		i;
+
+	threshold = INT_MIN;
+	next_min_idx = lst_min(lst, threshold);
+	i = 0;
+	while (next_min_idx >= 0)
+	{
+		item = (t_item *)(ft_lstidx(lst, next_min_idx)->content);
+		item->index = i++;
+		threshold = item->value;
+		next_min_idx = lst_min(lst, threshold);
 	}
 }
 
-void	lst_print(void *content)
+void	item_print(void *content)
 {
-	ft_printf("%i ", *(int *)content);
+	t_item	*item;
+
+	item = (t_item *)content;
+	ft_printf("[%i]:%i ", item->index, item->value);
 }
 
 int	main(int argc, char **argv)
 {
 	t_list	*lst;
-	int		*n;
+	t_item	*item;
 
 	lst = 0;
-	n = 0;
+	item = 0;
 	if (argc <= 1)
 	{
 		ft_printf("no args : (\n");
@@ -74,17 +85,14 @@ int	main(int argc, char **argv)
 	}
 	while (argc > 1)
 	{
-		n = ft_calloc(1, sizeof(int));
-		if (!n)
+		item = ft_calloc(1, sizeof(t_item));
+		if (!item)
 			return (1);
-		*n = ft_atoi(argv[argc - 1]);
-		ft_lstadd_front(&lst, ft_lstnew(n));
+		item->value = ft_atoi(argv[argc - 1]);
+		ft_lstadd_front(&lst, ft_lstnew(item));
 		--argc;
 	}
-	ft_printf("original: ");
-	ft_lstiter(lst, lst_print);
-	ft_printf("\nsorted: ");
-	lst_bubble(lst, lst_less);
-	ft_lstiter(lst, lst_print);
+	lst_fill_indices(lst);
+	ft_lstiter(lst, item_print);
 	ft_lstclear(&lst, free);
 }
---
+++ push_swap.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmoraled <dmoraled@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/04 11:21:27 by dmoraled          #+#    #+#             */
+/*   Updated: 2024/12/04 11:23:18 by dmoraled         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <limits.h>
+
+typedef struct s_item
+{
+	int	value;
+	int	index;
+}	t_item;
+
+#endif
