d25b998f1613e2a62dcf8d451b05f519d64c9cc9
biballo <dmoraleda11@gmail.com>
2024-12-26 14:24:42 +0100

    radix working

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: dmoraled <dmoraled@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 10:35:38 by dmoraled          #+#    #+#             */
-/*   Updated: 2024/12/17 10:39:10 by dmoraled         ###   ########.fr       */
+/*   Updated: 2024/12/26 14:23:12 by dmoraled         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 void	print_stack(t_list *a, t_list *b)
 {
+	ft_printf("---\n");
 	while (a || b)
 	{
 		if (a)
@@ -37,81 +38,89 @@ void	print_stack(t_list *a, t_list *b)
 
 int	lst_check_radix_val(t_list *lst, int dig, int val)
 {
-	int	ret;
 	int	item_dig;
 	
-	ret = 1;
 	while (lst) {
 		item_dig = (((t_item *)lst->content)->index & (1 << dig)) != 0;
-		if (item_dig ^ val)
-		{
-			ret = 0;
-			break ;
-		}
+		if (item_dig != val)
+			return (0);
 		lst = lst->next;
 	}
-	return ret;
+	return (1);
 }
 
 void	sort_iter(t_list **a, t_list **b, int dig)
 {
-	while (a && *a) {
-		if (lst_check_radix_val(*a, dig, 1))
-			break ;
-		if (!(((t_item *)(*a)->content)->index & (1 << dig))) {
+	int	v;
+	int i;
+
+	i = ft_lstsize(*a);
+	while (a && *a && i > 0) {
+		v = (((t_item *)(*a)->content)->index);
+		if (!(v & (1 << dig))) {
 			pb(a, b);
 		} else {
 			rs(a, 'a');
 		}
+		--i;
 	}
-	int b_rots = 0;
+	// print_stack(*a, *b);
 	while (b && *b) {
-		if (lst_check_radix_val(*b, dig, 0))
-			break ;
-		if ((((t_item *)(*b)->content)->index & (1 << dig))) {
-			pa(a, b);
-		} else {
-			rs(b, 'b');
-			b_rots++;
-		}
+		pa(a, b);
 	}
-	while (b_rots > 0) {
-		rrs(b, 'b');
-		--b_rots;
+}
+
+int	num_bin_digits(int n)
+{
+	int c;
+
+	c = 0;
+	while (n > 0)
+	{
+		n /= 2;
+		++c;
 	}
+	return c;
 }
 
 int	lst_max_binary_digits(t_list *lst)
 {
-	int	max = 0;
+	int	max;
+	int	i;
+	int	n;
 
+	max = 0;
 	while (lst)
 	{
-
+		i = ((t_item *)lst->content)->index;
+		n = num_bin_digits(i);
+		if (max < n)
+			max = n;
 		lst = lst->next;
 	}
+	return (max);
 }
 
 int	main(int argc, char **argv)
 {
 	t_list	*a;
 	t_list	*b;
+	int	iter;
 
 	a = parse_input(argc, argv);
 	if (!a)
 		return (1);
 	b = 0;
+	iter = lst_max_binary_digits(a);
 	int dig = 0;
-	print_stack(a, b);
-	sort_iter(&a, &b, dig);
-	print_stack(a, b);
-	++dig;
-	sort_iter(&a, &b, dig);
-	print_stack(a, b);
-	while (b) {
-		pa(&a, &b);
+	while (iter > 0)
+	{
+		// print_stack(a, b);
+		sort_iter(&a, &b, dig);
+		++dig;
+		--iter;
 	}
-	print_stack(a, b);
+	// print_stack(a, b);
 	ft_lstclear(&a, lst_item_free);
 	ft_lstclear(&b, lst_item_free);
 }
